d463b0cbe1313259ffca62cb4a9aa33929585fdc
Simon Rowe <simon@wiremoons.com>
2022-11-29 21:16:43 +0000

    Improve version output layout

--- lib/version.dart
+++ lib/version.dart
@@ -1,8 +1,6 @@
-
 import 'dart:io';
 
 class Version {
-
   late String _osVersion;
   late String _appVersion;
   late String _osName;
@@ -13,8 +11,10 @@ class Version {
   late String _systemLocale;
   late String _appLastModifiedOrBuilt;
   // Add a build time flag: dart compile exe -DDART_BUILD="Built on: $(date)" ./bin/dart_install.dart
-  static const String _buildEnvData = String.fromEnvironment("DART_BUILD", defaultValue: "");
-  static const String _buildMode = bool.fromEnvironment('dart.vm.product') ? "Release" : "Debug";
+  static const String _buildEnvData =
+      String.fromEnvironment("DART_BUILD", defaultValue: "");
+  static const String _buildMode =
+      bool.fromEnvironment('dart.vm.product') ? "Release" : "Debug";
   // alternative Flutter build mode needs: import 'package:flutter/foundation.dart';
   //static const _buildMode = (kReleaseMode) ? "Release" : "Debug";
 
@@ -23,18 +23,20 @@ class Version {
     _systemLocale = Platform.localeName;
     _appVersion = appVersion;
     _osVersion = Platform.operatingSystemVersion;
-    _appName =  _getAppName();
+    _appName = _getAppName();
     _dartVersion = Platform.version;
     _computerName = _getComputerName();
     _numberCpus = Platform.numberOfProcessors.toString();
-    _appLastModifiedOrBuilt = (_buildEnvData.isNotEmpty) ? _buildEnvData : _getAppLastModified();
+    _appLastModifiedOrBuilt =
+        (_buildEnvData.isNotEmpty) ? _buildEnvData : _getAppLastModified();
   }
 
   // formats operating system name
   String _toCapitaliseOS(String input) {
     if (input.isEmpty) return input;
     if (input.contains("macos")) {
-      return input.substring(0,3) + input.substring(3,input.length).toUpperCase();
+      return input.substring(0, 3) +
+          input.substring(3, input.length).toUpperCase();
     }
     return input[0].toUpperCase() + input.substring(1);
   }
@@ -46,23 +48,23 @@ class Version {
 
   // provide application name (exe) or filename (script)
   String _getAppName() {
-    return (_isScript()) ?
-    Platform.script.pathSegments.last :
-    Platform.resolvedExecutable.split("/").last;
+    return (_isScript())
+        ? Platform.script.pathSegments.last
+        : Platform.resolvedExecutable.split("/").last;
   }
 
   // applications full path and name (exe) or full path and filename (script)
   String _getAppFullPath() {
-    return (_isScript()) ?
-    Platform.script.toFilePath() :
-    Platform.resolvedExecutable;
+    return (_isScript())
+        ? Platform.script.toFilePath()
+        : Platform.resolvedExecutable;
   }
 
   // removes the '.local' on macos from the hostname
   String _getComputerName() {
-    return (Platform.localHostname.endsWith(".local")) ?
-    Platform.localHostname.split(".").first :
-    Platform.localHostname;
+    return (Platform.localHostname.endsWith(".local"))
+        ? Platform.localHostname.split(".").first
+        : Platform.localHostname;
   }
 
   // for a Dart script or where no '-DDART_BUILD' - get the last modified time stamp
@@ -72,14 +74,13 @@ class Version {
   }
 
   void display() {
-    stdout.writeln("""
-  '$_appName' is version: '$_appVersion' built in '$_buildMode' mode.
-  $_appLastModifiedOrBuilt
-  Running Dart version: '$_dartVersion'.
-  Executing on computer '$_computerName' with '$_osName $_osVersion' with $_numberCpus CPUs.
-  System locale is '$_systemLocale'.""");
+    stdout.writeln(
+        """\n'$_appName' is version: '$_appVersion' built in '$_buildMode' mode.
+$_appLastModifiedOrBuilt
+Running Dart version: '$_dartVersion'.
+Executing on computer '$_computerName' with '$_osName $_osVersion' with $_numberCpus CPUs.
+System locale is '$_systemLocale'.""");
   }
-
 }
 
 
